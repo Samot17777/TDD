8176d461181832a7934cb4fc3715d24f070ec0ce
Tomasz Wylężek <Tomasz.Wylezek@jcommerce.pl>
2016-09-22 10:27:47 +0200

    added testdouble

--- test/file/readFileTest.js
+++ test/file/readFileTest.js
@@ -1,18 +1,50 @@
 const assert = require('assert');
 const readFile = require('../../lib/file/readFile');
 const coMocha = require('co-mocha');
-const fs = require('fs');
+const td = require('testdouble');
+
 
 describe ('read file', () => {
     it('[]integration test should get file content', function * () {
+        const fs = require('fs');
+        //given
         const read = readFile({fs});
-
+        //when
         const result = yield read('./symbols.txt');
-
+        //then  
         assert.equal(result, 'GOOG\r\nAAPL\r\nORCL\r\nMSFT\r\nINVALID');
     });
 
+    it('[unit test] should get file content', function* () {
+        //given
+        const fs = {
+            readFile: function (fileName, encoding, callback) {
+                assert.equal(fileName, './symbols.txt');
+                assert.equal(encoding, 'utf8');
+                callback(null, 'content');
+            }
+        }
+        const read = readFile({fs}); 
+        //when
+        const result = yield read('./symbols.txt');
+        //then
+        assert.equal(result, 'content');
+
+    });
+    it('[unit test with mocking library] should get file content', function* () {
+        //given
+        const fs = td.object();
+        td.when(fs.readFile('./symbols.txt', 'utf8')).thenCallback(null, 'content');
+        const read = readFile({fs}); 
+        //when
+        const result = yield read('./symbols.txt');
+        //then
+        assert.equal(result, 'content');
+
+    });
+
     it('[]integration test should fail on nonexistant file', function * () {
+        const fs = require('fs');
         const read = readFile({fs});
         try {
             yield read('./symbolsesesfahfsguafuigsfjasfgifiugiojsoghash.txt_invalid');
