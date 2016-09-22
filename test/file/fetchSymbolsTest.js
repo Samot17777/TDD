ef3c2d809918a92dd79d899965d4f10d5bdc29e8
Tomasz Wylężek <Tomasz.Wylezek@jcommerce.pl>
2016-09-22 09:30:49 +0200

    adde another version of test

--- test/file/fetchSymbolsTest.js
+++ test/file/fetchSymbolsTest.js
@@ -1,8 +1,28 @@
-'use strict';
 const fetchSymbols = require('../../lib/file/fetchSymbols');
 const assert = require('assert');
+const td = require('testdouble');
 
 describe ('fetchSymbols e2e', () => {
+
+    if('should use biblioteka test double', function * () {
+        //given
+        const readFile = td.function('readFile');
+        td.when(readFile('someFile')).thenReturn(Promise.resolve('plik'));
+
+        const parseSymbols = td.function('parseSymbols');
+        td.when(parseSymbols('A,B')).thenReturn(['A', 'B']);
+
+        const fetch = fetchSymbols({ readFile, parseSymbols});
+        //when 
+        const symbols = yield fetch('plik');
+
+        //then 
+
+        td.verify(readFile('plik'));
+        td.verify(parseSymbols('A,B'));
+        assert.deepEqual(symbols, ['A', 'B']);
+    });
+
     it('should pass ? ',   function * () {
         //given
         let expectedAssertionCount = 0;
@@ -27,4 +47,6 @@ describe ('fetchSymbols e2e', () => {
         expectedAssertionCount++;
         assert.equal(expectedAssertionCount, 3, 'expected number of assertions');
     });
+
+
 });
