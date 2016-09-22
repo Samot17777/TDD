16aef7295324df937c2fe9cb56b13ba8a1a6ef34
Tomasz Wylężek <Tomasz.Wylezek@jcommerce.pl>
2016-09-22 12:44:19 +0200

    added new test

--- test/http/readAllPricesTest.js
+++ test/http/readAllPricesTest.js
@@ -5,14 +5,34 @@ const coMocha = require('co-mocha');
 
 
 describe ('read all prices', () => {
-    it('[integration test] should return some 200s', function * () {
+    it.skip('[integration test] should return some 200s TRUE STORY IT REALLY SHOOT', function * () {
         const request = require('good-guy-http')();
+
+
         //given
         const readPrices = readAllPrices({request});
         //when
         const result = yield readPrices(['GOOG','AAPL','Allahakbar']);
         //then  
-        result.forEach( el => assert.equal(el.statusCode, 200));
+        assert.equal(result[0].statusCode, 200);
+        assert.equal(result[1].statusCode, 200);
+        assert.equal(result[2].statusCode, 404);
     });
+    it('[integration test] should return some 200s or 404 or what ?', function * () {
+        function request(url) {
+             return url.endsWith('INVALID') ?  Promise.reject({statusCode : 404}) : Promise.resolve({statusCode: 200});
+        }
+
+
+        //given
+        const readPrices = readAllPrices({request});
+        //when
+        const result = yield readPrices(['GOOG','AAPL','INVALID']);
+        //then  
+        assert.equal(result[0].statusCode, 200);
+        assert.equal(result[1].statusCode, 200);
+        assert.equal(result[2].statusCode, 404);
+    });
+
 
 });
